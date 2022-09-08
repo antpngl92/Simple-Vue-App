1de6a571ea0b438562f3f6bc612c55b054a1fcb3
Anton <nyagolov.anton@gmail.com>
2022-09-08 16:06:12 +0300

    Remove not found component from router

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory, NotFoundComponent } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import EventList from '../views/EventList.vue'
 import EventDetails from '../views/EventDetails.vue'
 import About from '../views/About.vue'
@@ -20,10 +20,6 @@ const routes = [
     path: '/about',
     name: 'About',
     component: About
-  },
-  {
-    path: '/*',
-    component: NotFoundComponent
   }
 ]
 

f045d19e018adbf114bc6ec1358a9959ea00d347
Anton <nyagolov.anton@gmail.com>
2022-09-09 15:02:12 +0300

    Introduce global flashMessage object

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
-import { createApp } from 'vue'
+import { createApp, reactive } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// Global Store object
+const GStore = reactive({ flashMessage: '' })
+
 createApp(App)
   .use(store)
   .use(router)
+  .provide('GStore', GStore) // Makes GStore Object available for any component in our app to use
   .mount('#app')

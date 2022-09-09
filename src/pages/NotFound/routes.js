0287a3ec47d2bc6192ce11417cf2217dd4a52883
Anton <nyagolov.anton@gmail.com>
2022-09-09 14:35:56 +0300

    Introduce 404 for resource

--- src/pages/NotFound/routes.js
+++ src/pages/NotFound/routes.js
@@ -1,10 +1,18 @@
 import NotFound from '@/pages/NotFound/NotFound.vue'
 
 const routes = [
+  // pages
   {
     path: '/:catchAll(.*)',
     name: 'NotFound',
     component: NotFound
+  },
+  // resources
+  {
+    path: '/404/:resource',
+    name: '404Resource',
+    component: NotFound,
+    props: true
   }
 ]
 
